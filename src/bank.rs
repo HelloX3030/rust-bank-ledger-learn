733fad9635a8996b0cfe58dab6ecace8728179a2
lseeger <lseeger@student.42.fr>
2025-07-17 17:25:04 +0200

    added accounts deletion logic

--- src/bank.rs
+++ src/bank.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use crate::account::{Account};
+use crate::account::Account;
 
 pub struct Bank{
     next_id: u64,
@@ -12,6 +12,10 @@ impl Bank {
     }
 
     pub fn print(&self) {
+        println!("==================================================");
+        if self.accounts.is_empty() {
+            println!("No Accounts Found!");
+        }
         for i in 0..self.next_id{
             if let Some(account) = self.accounts.get(&i) {
                 account.print();
@@ -24,5 +28,25 @@ impl Bank {
         acc.id = self.next_id;
         self.next_id += 1;
         self.accounts.insert(acc.id, acc);
+    }
+
+    pub fn get_id(&self, name: String) -> Option<u64>
+    {
+        for (id, account) in &self.accounts {
+            if account.name == name {
+                return Some(*id);
+            }
+        }
+        None
+    }
+
+    pub fn delete_account(&mut self, name: String) {
+        if let Some(id) = self.get_id(name) {
+            self.accounts.remove(&id);
+        }
+    }
+
+    pub fn delete_account_id(&mut self, id: u64) {
+        self.accounts.remove(&id);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ use bank::Bank;
 
 fn main() {
     let mut bank = Bank::new();
+    bank.print();
     bank.add_account(String::from("Spaten"));
     bank.add_account(String::from("Spaten 2"));
     bank.add_account(String::from("Spaten 3"));
     bank.add_account(String::from("5555"));
     bank.add_account(String::from("5734958234"));
+    bank.print();
+    bank.delete_account_id(1);
+    bank.delete_account(String::from("5555"));
     bank.print();
 }
